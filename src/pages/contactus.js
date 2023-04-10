56cce3d134e79865c5e5b77d808fbb63ea85fc1c
jyancey27 <jaydonyancey7@gmail.com>
2023-04-10 10:03:58 -0700

    bug fixes

--- src/pages/contactus.js
+++ src/pages/contactus.js
@@ -1,79 +1,52 @@
 
-import React from 'react';
-import {useState} from 'react';
+import React, {useState} from 'react';
 import Axios from "axios";
   
 const ContactUs = () => {
 
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [email, setEmail] = useState('');
-  const [reason, setReason] = useState('');
-  const [preference, setPreference] = useState('email');
-  const [isDisabled, setDisabled] = useState(false);
-  const [checkedPhoneNumber, setCheckedPhoneNumber] = useState(false);
-  const [checkedEmail, setCheckedEmail] = useState(true);
+  const [user, setUser] = useState({
+    firstName: '',
+    lastName: '',
+    phoneNumber: '',
+    email: '',
+    reason: '',
+    preference: 'email'
+  });
 
+  const [isDisabled, setDisabled] = useState(false);
 
 
   const clearFields = () => {
-    setFirstName('');
-    setLastName('');
-    setPhoneNumber('');
-    setEmail('');
-    setReason('');
-    setCheckedPhoneNumber(false);
-    setCheckedEmail(true);
+    setUser({
+      ...user,
+      firstName: '',
+      lastName: '',
+      phoneNumber: '',
+      email: '',
+      reason: ''
+    });
   };
 
 
-  const firstNameChange = event => {
-      setFirstName(event.target.value);
-  };
 
-  const lastNameChange = event => {
-      setLastName(event.target.value);
+  const handleInputChange = event => {
+    const { name, value } = event.target;
+    setUser(prevState => ({ ...prevState, [name]: value }));
   };
-
-  const phoneNumberChange = event => {
-    setPhoneNumber(event.target.value);
-  };
-
-  const emailChange = event => {
-      setEmail(event.target.value);
-  };
-
-  const reasonChange = event => {
-    setReason(event.target.value);
-  };
-
-  const preferenceChange = event => {
-    if (event.target.value === 'email') {
-      setCheckedEmail(true);
-      setCheckedPhoneNumber(false);
-      setPreference(event.target.value);
-    } else {
-      setCheckedPhoneNumber(true);
-      setCheckedEmail(false);
-      setPreference(event.target.value);
-    };
-  };
-
 
 
   const postInfo = () => {
-    setDisabled(true);
+    setDisabled(isDisabled => !isDisabled);
 
-    if (email !== '' && email.includes('@') && reason !== '') {
+    if (user.email !== '' && user.email.includes('@') && user.reason !== '') {
 
       const reqBody = {
-      "firstName": firstName,
-      "lastName": lastName,
-      "phoneNumber": phoneNumber,
-      "email": email,
-      "reason": reason,
-      "preference": preference,
+      "firstName": user.firstName,
+      "lastName": user.lastName,
+      "phoneNumber": user.phoneNumber,
+      "email": user.email,
+      "reason": user.reason,
+      "preference": user.preference,
       };
 
       Axios.post("http://localhost:3002/postInfo", reqBody).then(() => {
@@ -97,28 +70,45 @@ const ContactUs = () => {
           <form>
           <div align="center">
             <div>
-                <input type='text' id='firstName' name='firstName' onChange={firstNameChange} value={firstName} placeholder='First Name' maxLength='50'></input>
+                <input type='text' id='firstName' name='firstName' onChange={handleInputChange} value={user.firstName} placeholder='First Name' maxLength='50'></input>
               </div>
             <div>
-                <input type='text' id='lastName' name='lastName' onChange={lastNameChange} value={lastName} placeholder='Last Name' maxLength='50'></input>
+                <input type='text' id='lastName' name='lastName' onChange={handleInputChange} value={user.lastName} placeholder='Last Name' maxLength='50'></input>
               </div>
             <div>
-              <input type='text' id='phoneNumber' name='phoneNumber' onChange={phoneNumberChange} value={phoneNumber} placeholder='Phone Number' maxLength='16'></input>
+              <input type='text' id='phoneNumber' name='phoneNumber' onChange={handleInputChange} value={user.phoneNumber} placeholder='Phone Number' maxLength='16'></input>
             </div>
             <div>
-              <input type='text' id='email' name='email' onChange={emailChange} value={email} placeholder='Email Address' maxLength='100' required></input>
+              <input type='text' id='email' name='email' onChange={handleInputChange} value={user.email} placeholder='Email Address' maxLength='100' required></input>
             </div>
             <div>
-              <input type='text' id='reason' name='reason' onChange={reasonChange} value={reason} placeholder='Reason Why' maxLength='250' required></input>
+              <input type='text' id='reason' name='reason' onChange={handleInputChange} value={user.reason} placeholder='Reason Why' maxLength='250' required></input>
             </div>
             <div>
               <p>Contact Preference</p>
             </div>
             <div>
-              <input type='radio' id='emailPreference' name='emailPreference' onChange={preferenceChange} value='email' checked={checkedEmail}></input>
-              <label>Email</label>
-              <input type='radio' id='phoneNumberPreference' name='phoneNumberPreference' onChange={preferenceChange} value='phone number' checked={checkedPhoneNumber}></input>
-              <label>Phone</label> 
+              <label>
+                <input
+                  type="radio"
+                  name="preference"
+                  value="email"
+                  checked={user.preference === "email"}
+                  onChange={handleInputChange}
+                />
+                Email
+              </label>
+
+              <label>
+                <input
+                  type="radio"
+                  name="preference"
+                  value="phone"
+                  checked={user.preference === "phone"}
+                  onChange={handleInputChange}
+                />
+                Phone
+              </label>
             </div>
           </div> <br></br>
           <button className='button' onClick={postInfo} disabled={isDisabled} ><strong>Submit</strong></button>
